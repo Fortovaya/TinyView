d30b55eed2edd1711e7562b9362f609db12dc668
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-25 18:58:23 +0300

    [Feature] Init project TinyView

---
+++ TinyView/TinyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyViewApp.swift
+//  TinyView
+//
+//  Created by Алина on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TinyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
