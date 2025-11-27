6ac5a859072f724b623013504b16c63afcd2452f
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-27 19:58:35 +0300

    [Refactor] Update global UI appearance for TabBar and NavigationBar

---
+++ TinyView/App/TinyViewApp.swift
@@ -0,0 +1,39 @@
+//
+//  TinyViewApp.swift
+//  TinyView
+//
+//  Created by Алина on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TinyViewApp: App {
+
+	init() {
+		let tabAppearance = UITabBarAppearance()
+		tabAppearance.configureWithOpaqueBackground()
+		tabAppearance.backgroundColor = .systemBackground
+		tabAppearance.shadowColor = .clear
+		UITabBar.appearance().standardAppearance = tabAppearance
+		UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+
+		let navAppearance = UINavigationBarAppearance()
+		navAppearance.configureWithOpaqueBackground()
+		navAppearance.backgroundColor = .systemBackground
+		navAppearance.shadowColor = .clear
+		navAppearance.shadowImage = UIImage()
+		UINavigationBar.appearance().standardAppearance = navAppearance
+		UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+		UINavigationBar.appearance().compactAppearance = navAppearance
+
+		UITableView.appearance().backgroundColor = .clear
+		UITableViewCell.appearance().backgroundColor = .clear
+	}
+
+    var body: some Scene {
+        WindowGroup {
+			RootView()
+        }
+    }
+}
