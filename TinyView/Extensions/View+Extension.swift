30e956330de7a4871b3007066f8b0f260ccb2243
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-27 20:25:10 +0300

    [Feature] Add global keyboard dismissal helpers

---
+++ TinyView/Extensions/View+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  View+Extension.swift
+//  TinyView
+//
+//  Created by Алина on 27.11.2025.
+//
+
+import SwiftUI
+
+extension View {
+	func dismissKeyboard() {
+		UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+	}
+
+	func dismissKeyboardOnTap() -> some View {
+		self.onTapGesture {
+			self.dismissKeyboard()
+		}
+	}
+}
