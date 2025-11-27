7abefb0f9bc0f2685089a49c13299561e78a74cd
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-27 20:03:45 +0300

    [Feature] Implement MainView layout

---
+++ TinyView/Screens/Main/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  TinyView
+//
+//  Created by Алина on 26.11.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+	@StateObject private var editorViewModel = PDFEditorViewModel()
+	@StateObject private var documentsViewModel = DocumentsListViewModel()
+
+	var body: some View {
+		NavigationView {
+			TabView {
+				PDFEditorView(viewModel: editorViewModel)
+					.tabItem {
+						Label("Создать", systemImage: "plus.square.on.square")
+					}
+
+				DocumentsListView(viewModel: documentsViewModel)
+					.tabItem {
+						Label("Документы", systemImage: "doc.richtext")
+					}
+			}
+		}
+		.navigationViewStyle(.stack)
+	}
+}
