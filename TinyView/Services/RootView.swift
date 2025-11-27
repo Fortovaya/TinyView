b25fe2bfd4dbb21e0c75bac137b7b27a01aab310
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-27 20:21:06 +0300

    [Refactor] Add RootView

---
+++ TinyView/Services/RootView.swift
@@ -0,0 +1,22 @@
+//
+//  RootView.swift
+//  TinyView
+//
+//  Created by Алина on 25.11.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+	@AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
+
+	var body: some View {
+		NavigationView {
+			if hasCompletedOnboarding {
+				MainView()
+			} else {
+				WelcomeScreenView(hasCompletedOnboarding: $hasCompletedOnboarding)
+			}
+		}
+	}
+}
